9c393f2c8ad2dad95b8d46d457aa969bcd032e31
Mujaffar Husain <93472360+mujhusain@users.noreply.github.com>
2023-05-26 13:52:07 +0530

    analog clock added

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -3,26 +3,35 @@ import { Button, StyleSheet } from "react-native";
 import { Text, View } from "components/themed";
 import { useThemeColors } from "src/hooks/useThemeColors";
 import { useEffect, useState } from "react";
+import AnalogClock from "src/components/anlogClock/AnalogClock";
 
 export default () => {
   const { colors } = useThemeColors();
-  const [currentTime, setCurrentTime] = useState('');
+  const [currentTime, setCurrentTime] = useState("");
   const [is24HourFormat, setIs24HourFormat] = useState(true);
 
   useEffect(() => {
     const interval = setInterval(() => {
       const date = new Date();
-      
+
       let hours = date.getHours();
       let minutes = date.getMinutes();
       let seconds = date.getSeconds();
-      
+
       if (!is24HourFormat) {
-        const suffix = hours >= 12 ? 'PM' : 'AM';
+        const suffix = hours >= 12 ? "PM" : "AM";
         hours = hours % 12 || 12;
-        setCurrentTime(`${hours}:${minutes < 10 ? '0' + minutes : minutes}:${seconds < 10 ? '0' + seconds : seconds} ${suffix}`);
+        setCurrentTime(
+          `${hours}:${minutes < 10 ? "0" + minutes : minutes}:${
+            seconds < 10 ? "0" + seconds : seconds
+          } ${suffix}`
+        );
       } else {
-        setCurrentTime(`${hours < 10 ? '0' + hours : hours}:${minutes < 10 ? '0' + minutes : minutes}:${seconds < 10 ? '0' + seconds : seconds}`);
+        setCurrentTime(
+          `${hours < 10 ? "0" + hours : hours}:${
+            minutes < 10 ? "0" + minutes : minutes
+          }:${seconds < 10 ? "0" + seconds : seconds}`
+        );
       }
     }, 1000);
 
@@ -30,15 +39,16 @@ export default () => {
   }, [is24HourFormat]);
 
   const toggleTimeFormat = () => {
-    setIs24HourFormat(prevFormat => !prevFormat);
+    setIs24HourFormat((prevFormat) => !prevFormat);
   };
 
   return (
     <View style={styles.container}>
-      <Text style={[styles.text, { color: colors.textSubtle }]}>Time</Text>
+      <Text style={[styles.text, { color: colors.textSubtle }]}>India</Text>
       <Text style={styles.timeText}>{currentTime}</Text>
+      <AnalogClock />
       <Button
-        title={is24HourFormat ? 'Switch to 12-hour' : 'Switch to 24-hour'}
+        title={is24HourFormat ? "Switch to 12-hour" : "Switch to 24-hour"}
         onPress={toggleTimeFormat}
       />
     </View>
@@ -49,7 +59,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    justifyContent:"space-evenly",
+    justifyContent: "space-evenly",
+    paddingBottom:80
   },
   timeText: {
     fontSize: 60,
